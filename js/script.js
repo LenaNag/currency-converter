a803b5bd49010b25fcbd258a09e1f5f8b127fff3
Alena Nahornaya <nahornaya.alena@gmail.com>
2024-01-01 19:40:37 +0100

    all 'let' variables changed on 'const' variables where it's necessary

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
-let usd = 3.94;
-let eur = 4.36;
-let amountElement = document.querySelector(".js-form__field-amount");
-let currencyElement = document.querySelector(".js-form__field-currency");
-let formElement = document.querySelector(".js-form");
-let resultElement = document.querySelector(".js-form__result");
+{
+    const usd = 3.94;
+    const eur = 4.36;
+    const amountElement = document.querySelector(".js-form__field-amount");
+    const currencyElement = document.querySelector(".js-form__field-currency");
+    const formElement = document.querySelector(".js-form");
+    const resultElement = document.querySelector(".js-form__result");
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-    let amount = +amountElement.value;
-    let currency = currencyElement.value;
-    let result;
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
+        let result;
 
-    switch (currency) {
-        case "USD":
-            result = amount / usd;
-            break;
-        case "EUR":
-            result = amount / eur;
-            break;
-    }
-    resultElement.innerText = `${amount} PLN = ${result.toFixed(2)} ${currency}`;
-});
+        switch (currency) {
+            case "USD":
+                result = amount / usd;
+                break;
+            case "EUR":
+                result = amount / eur;
+                break;
+        }
+        resultElement.innerText = `${amount} PLN = ${result.toFixed(2)} ${currency}`;
+    });
+}
