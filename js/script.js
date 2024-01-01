77b8daa59f5f2f6018932ee7abd0e815a9ac3f4f
Alena Nahornaya <nahornaya.alena@gmail.com>
2024-01-01 20:04:49 +0100

    js functions changed to arrow functions

--- js/script.js
+++ js/script.js
@@ -1,26 +1,40 @@
 {
-    const usd = 3.94;
-    const eur = 4.36;
-    const amountElement = document.querySelector(".js-form__field-amount");
-    const currencyElement = document.querySelector(".js-form__field-currency");
-    const formElement = document.querySelector(".js-form");
-    const resultElement = document.querySelector(".js-form__result");
-
-    formElement.addEventListener("submit", (event) => {
-        event.preventDefault();
-
-        const amount = +amountElement.value;
-        const currency = currencyElement.value;
-        let result;
+    const calculateResult = (amount, currency) => {
+        const usd = 3.94;
+        const eur = 4.36;
 
         switch (currency) {
             case "USD":
-                result = amount / usd;
-                break;
+                return amount / usd;
+
             case "EUR":
-                result = amount / eur;
-                break;
+                return amount / eur;
         }
+    };
+
+    const updateResultText = (amount, result, currency) => {
+        const resultElement = document.querySelector(".js-form__result");
         resultElement.innerText = `${amount} PLN = ${result.toFixed(2)} ${currency}`;
-    });
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const amountElement = document.querySelector(".js-form__field-amount");
+        const currencyElement = document.querySelector(".js-form__field-currency");
+
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
+
+        const result = calculateResult(amount, currency);
+        updateResultText(amount, result, currency);
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
 }
